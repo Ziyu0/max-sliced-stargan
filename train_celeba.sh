1cc41584c89deb929bae017950908589d8a8fb2b
Ziyu0 <yuliazhouziyu@gmail.com>
2019-04-05 15:04:40 -0500

    Fix bug in bash script

--- train_celeba.sh
+++ train_celeba.sh
@@ -15,7 +15,7 @@ python main.py \
 --image_size $IMAGE_SIZE \
 --sample_dir "$EXP_ROOT_DIR/samples" \
 --log_dir "$EXP_ROOT_DIR/logs" \
---model_save_dir "$EXP_ROOT_DIR/models" 
+--model_save_dir "$EXP_ROOT_DIR/models" \
 --result_dir "$EXP_ROOT_DIR/results" \
 --config_dir "$EXP_ROOT_DIR/configs" \
 --c_dim $C_DIM \
