e86a2a5d831f5ddb440440f56c9ef36281702652
Ziyu0 <yuliazhouziyu@gmail.com>
2019-04-07 16:21:29 -0500

    Update swd

--- dist_funcs.py
+++ dist_funcs.py
@@ -7,20 +7,18 @@ def sliced_wasserstein_distance(true_samples, fake_samples, num_projections):
     """Compute Sliced Wasserstein Distance between real samples and
     generated samples.
 
-    TODO: before passing into this funciton, flattern 
-        feature_size = width * height * 3
-        or change dimension inside of this function
-        => Need to check the dimension of images provided by data loader
-
     Args:
-        true_samples: Samples from the real dataset, shape
-            (N, num_features), where N = num of samples or batch size
-        fake_samples: Samples from the generator, shape
-            (N, num_features), where N = num of samples or batch size
+        true_samples: Samples from the real dataset, shape (N, C, H, W)
+        fake_samples: Samples from the generator, shape (N, C, H, W)
     Returns:
         Sliced Wasserstein Distance, a scalar
     """
-    num_features = true_samples.shape[1]
+    # Reshape samples into (N, num_features)
+    _, channels, height, width = true_samples.shape
+    num_features = channels * height * width
+
+    true_samples = true_samples.view(-1, num_features)
+    fake_samples = fake_samples.view(-1, num_features)
 
     # Random projection directions, shape (num_features, num_projections)
     projections = np.random.normal(size=(num_features, num_projections))
